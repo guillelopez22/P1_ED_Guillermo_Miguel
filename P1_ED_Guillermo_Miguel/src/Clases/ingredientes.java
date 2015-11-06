014f0a2ccb451b513750bef4e194477e6148369d
Miguel Angel Ardon Enriquez <Miguel Angel Ardon Enriquez>
2015-11-05 18:14:21 -0600

    Agregando ordenes

--- P1_ED_Guillermo_Miguel/src/Clases/ingredientes.java
+++ P1_ED_Guillermo_Miguel/src/Clases/ingredientes.java
@@ -32,4 +32,3 @@ public class ingredientes {
         return "ingredientes{" + "nombre=" + nombre + '}';
     }
 } 
-}
--- P1_ED_Guillermo_Miguel/src/Clases/menu.java
+++ P1_ED_Guillermo_Miguel/src/Clases/menu.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
-
-/**
- *
- * @author Miguel A. Ardon E
- */
 public class menu {
-    
+    String nombre;
+    String descrpcion;
+    int precio;
+    Lista ingrediente;
+    int timpo;
+
+    public menu() {
+    }
+
+    public menu(String nombre, String descrpcion, int precio, Lista ingrediente, int timpo) {
+        this.nombre = nombre;
+        this.descrpcion = descrpcion;
+        this.precio = precio;
+        this.ingrediente = ingrediente;
+        this.timpo = timpo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrpcion() {
+        return descrpcion;
+    }
+
+    public void setDescrpcion(String descrpcion) {
+        this.descrpcion = descrpcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public Lista getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Lista ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public int getTimpo() {
+        return timpo;
+    }
+
+    public void setTimpo(int timpo) {
+        this.timpo = timpo;
+    }
+
+    @Override
+    public String toString() {
+        return "menu{" + "nombre=" + nombre + '}';
+    }
 }
