16f3e048152dc81083bb3315294d6dfe7320215f
Miguel Angel Ardon Enriquez <Miguel Angel Ardon Enriquez>
2015-11-05 18:29:07 -0600

    Agregando nodo y lista

---
+++ P1_ED_Guillermo_Miguel/src/Clases/Nodo.java
@@ -0,0 +1,30 @@
+package Clases;
+public class Nodo {
+    private Object object;
+    Nodo next;
+    
+    public Nodo(Object object){
+        this.object=object;
+    }
+    
+    public Nodo(){
+        
+    }
+            
+
+    public String getValue() {
+        return object.toString();
+    }
+
+    public void setValue(Object object) {
+        this.object = object;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+}
