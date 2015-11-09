b3457782b2fd896bde4f7b3aa2bd552de233bac8
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2015-11-09 16:24:57 -0600

    Agregando clase pila

--- P1_ED_Guillermo_Miguel/src/Clases/Pila.java
+++ P1_ED_Guillermo_Miguel/src/Clases/Pila.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
 
-/**
- *
- * @author Miguel A. Ardon E
- */
 public class Pila {
-    
+
+    Nodo_Ingrediente tos;
+    Nodo_Ingrediente head;
+    int size = 0;
+
+    public void push(ingredientes ingredientes) {
+        Nodo_Ingrediente newNodo = new Nodo_Ingrediente();
+        if (tos == null) {
+            tos = newNodo;
+        } else {
+            newNodo.next = tos;
+            tos = newNodo;
+        }
+        size++;
+    }
+
+    public ingredientes pop() {
+        ingredientes ingredientes;
+        if (tos == null) {
+            return null;
+        }
+        ingredientes = tos.getValue();
+        tos = tos.next;
+        size--;
+        return ingredientes;
+    }
+
+    public ingredientes tos() {
+
+        if (tos == null) {
+            return null;
+        } else {
+            return tos.getValue();
+        }
+    }
+
 }
